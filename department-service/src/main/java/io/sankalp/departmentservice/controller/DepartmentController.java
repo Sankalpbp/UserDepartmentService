46df9dc271a930b6ba2c45a5b1a51198aa74945c
Sankalpbp <sankalpnitj@gmail.com>
2022-11-23 06:52:41 +0530

    Setting up the APIs for Post and Get for the departments

--- department-service/src/main/java/io/sankalp/departmentservice/controller/DepartmentController.java
+++ department-service/src/main/java/io/sankalp/departmentservice/controller/DepartmentController.java
@@ -1,14 +1,29 @@
 package io.sankalp.departmentservice.controller;
 
+import io.sankalp.departmentservice.entity.Department;
 import io.sankalp.departmentservice.service.DepartmentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping ( "/departments" )
+@Slf4j
 public class DepartmentController {
 
     @Autowired
     private DepartmentService service;
+
+    @PostMapping ( "/" )
+    public Department saveDepartment ( @RequestBody Department department ) {
+        log.info ( "Inside saveDepartment () method of DepartmentController." );
+        return service.saveDepartment ( department );
+    }
+
+    @GetMapping ( "/{id}" )
+    public Department findDepartmentById ( @PathVariable ( "id" ) Long departmentId ) {
+        log.info ( "Side findDepartmentById () method in DepartmentController " );
+        return service.findDepartmentById ( departmentId );
+    }
+
 }
--- department-service/src/main/java/io/sankalp/departmentservice/service/DepartmentService.java
+++ department-service/src/main/java/io/sankalp/departmentservice/service/DepartmentService.java
@@ -1,12 +1,25 @@
 package io.sankalp.departmentservice.service;
 
+import io.sankalp.departmentservice.entity.Department;
 import io.sankalp.departmentservice.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class DepartmentService {
 
     @Autowired
     private DepartmentRepository repository;
+
+    public Department saveDepartment(Department department) {
+        log.info ( "Inside saveDepartment () method of DepartmentService." );
+        return repository.save ( department );
+    }
+
+    public Department findDepartmentById(Long departmentId) {
+        log.info ( "Inside findDepartmentById () method in DepartmentRespository " );
+        return repository.findByDepartmentId ( departmentId );
+    }
 }
