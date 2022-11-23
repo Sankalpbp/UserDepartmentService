fe35c8ec62cf2a24e6d1249292563c4a4a1b3522
Sankalpbp <sankalpnitj@gmail.com>
2022-11-23 06:58:16 +0530

    Setting up the files for entities, repositories, services and controllers.

---
+++ user-service/src/main/java/io/sankalp/userservice/service/UserService.java
@@ -0,0 +1,14 @@
+package io.sankalp.userservice.service;
+
+import io.sankalp.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+
+}
